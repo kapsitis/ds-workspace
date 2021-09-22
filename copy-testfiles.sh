e30c4d66951fa6cefc76c9d755bec0739fcc5b73
Kalvis Apsitis <liliyang146@gmail.com>
2021-09-22 08:46:36 +0300

    bash scripts

---
+++ copy-testfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp lab1-1/Makefile ../$1/lab1-1/
+cp lab1-1/test*.txt ../$1/lab1-1/
+cp lab1-1/expected*.txt ../$1/lab1-1/
+#cp lab1-2/test*.txt ../$1/lab1-2/
+cp run-lab1-tests.sh ../$1/
+
