a00c07f01ff236806043a71a16a9ba51f7dc93b2
kapsitis <kalvis.apsitis@gmail.com>
2021-09-27 01:32:21 +0300

    lab2-1 files added

--- lab2-1/test/test_CircularList.cpp
+++ lab2-1/test/test_CircularList.cpp
@@ -6,92 +6,91 @@ using namespace ds_course;
 TEST_CASE("circular_lists insert stuff in reverse order", "[circlelist]")
 {
     CircularList cL;
-    REQUIRE(cL.to_str() == "");
-    cL.add(2);
+    REQUIRE(cL.toString() == "");
+    cL.pushFront(2);
     REQUIRE(cL.getSize() == 1);
-    REQUIRE(cL.to_str() == "2");
-    cL.add(4);
+    REQUIRE(cL.toString() == "2");
+    cL.pushFront(4);
     REQUIRE(cL.getSize() == 2);
-    REQUIRE(cL.to_str() == "4 2");
-    cL.add(6);
+    REQUIRE(cL.toString() == "4 2");
+    cL.pushFront(6);
     REQUIRE(cL.getSize() == 3);
-    REQUIRE(cL.to_str() == "6 4 2");
+    REQUIRE(cL.toString() == "6 4 2");
 }
 
 TEST_CASE("On 6-number list:", "[circlelist]")
 {
     CircularList cl;
-    cl.add(11);
-    cl.advance();
-    cl.add(12);
-    cl.advance();
-    cl.add(13);
-    cl.advance();
-    cl.add(14);
-    cl.advance();
-    cl.add(15);
-    cl.advance();
-    cl.add(16);
-    cl.advance();
-    REQUIRE(cl.to_str() == "11 12 13 14 15 16");
+    cl.pushFront(11);
+    cl.moveNext();
+    cl.pushFront(12);
+    cl.moveNext();
+    cl.pushFront(13);
+    cl.moveNext();
+    cl.pushFront(14);
+    cl.moveNext();
+    cl.pushFront(15);
+    cl.moveNext();
+    cl.pushFront(16);
+    cl.moveNext();
+    REQUIRE(cl.toString() == "11 12 13 14 15 16");
 
     SECTION("Can advance 4 times") {
         for (int j = 0; j < 4; j++) 
-            cl.advance();
-        REQUIRE(cl.to_str() == "15 16 11 12 13 14");
+            cl.moveNext();
+        REQUIRE(cl.toString() == "15 16 11 12 13 14");
     }
 
     SECTION("Insert at position 2") {
-        cl.advance();
-        cl.advance();
-        REQUIRE(cl.to_str() == "13 14 15 16 11 12");
-        cl.add(1000);
-        REQUIRE(cl.to_str() == "1000 13 14 15 16 11 12");
-        cl.advance();
-        cl.advance();
-        cl.advance();
-        cl.advance();
-        cl.advance();
-        REQUIRE(cl.to_str() == "11 12 1000 13 14 15 16");
+        cl.moveNext();
+        cl.moveNext();
+        REQUIRE(cl.toString() == "13 14 15 16 11 12");
+        cl.pushFront(1000);
+        REQUIRE(cl.toString() == "1000 13 14 15 16 11 12");
+        cl.moveNext();
+        cl.moveNext();
+        cl.moveNext();
+        cl.moveNext();
+        cl.moveNext();
+        REQUIRE(cl.toString() == "11 12 1000 13 14 15 16");
     }
 
     SECTION("Delete at position 2") {
-        cl.advance();
-        cl.advance();
-        REQUIRE(cl.to_str() == "13 14 15 16 11 12");
-        cl.remove();
-        REQUIRE(cl.to_str() == "14 15 16 11 12");
-        cl.advance();
-        cl.advance();
-        cl.advance();
-        REQUIRE(cl.to_str() == "11 12 14 15 16");
+        cl.moveNext();
+        cl.moveNext();
+        REQUIRE(cl.toString() == "13 14 15 16 11 12");
+        cl.popFront();
+        REQUIRE(cl.toString() == "14 15 16 11 12");
+        cl.moveNext();
+        cl.moveNext();
+        cl.moveNext();
+        REQUIRE(cl.toString() == "11 12 14 15 16");
     }
 
     SECTION("insert, then delete") {
-        cl.add(100);
-        REQUIRE(cl.to_str() == "100 11 12 13 14 15 16");
-        cl.advance();
-        cl.advance();
-        cl.remove();
-        cl.advance();
-        cl.advance();
-        cl.advance();
-        cl.advance();
-        REQUIRE(cl.to_str() == "100 11 13 14 15 16");
+        cl.pushFront(100);
+        REQUIRE(cl.toString() == "100 11 12 13 14 15 16");
+        cl.moveNext();
+        cl.moveNext();
+        cl.popFront();
+        cl.moveNext();
+        cl.moveNext();
+        cl.moveNext();
+        cl.moveNext();
+        REQUIRE(cl.toString() == "100 11 13 14 15 16");
     }
 
     SECTION("ability to delete everything")
     {
-        cl.remove();
-        cl.remove();
-        cl.remove();
-        REQUIRE(cl.to_str() == "14 15 16");
-        cl.advance();
-        cl.remove();
-        cl.remove();
-        cl.remove();
-        REQUIRE(cl.to_str() == "");
-        cl.add(17);
-        REQUIRE(cl.to_str() == "17");
+        cl.popFront();
+        cl.popFront();
+        cl.popFront();
+        REQUIRE(cl.toString() == "14 15 16");
+        cl.popFront();
+        cl.popFront();
+        cl.popFront();
+        REQUIRE(cl.toString() == "");
+        cl.pushFront(17);
+        REQUIRE(cl.toString() == "17");
     }
 }
