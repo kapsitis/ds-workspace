67cc393a4ad98803fc6a91556bba359fb3236aab
kapsitis <kalvis.apsitis@gmail.com>
2021-09-29 00:25:28 +0300

    Fixed testcases

--- lab2-1/test/test_CircularList.cpp
+++ lab2-1/test/test_CircularList.cpp
@@ -6,16 +6,16 @@ using namespace ds_course;
 TEST_CASE("circular_lists insert stuff in reverse order", "[circlelist]")
 {
     CircularList cL;
-    REQUIRE(cL.toString() == "");
+    REQUIRE(cL.toString() == "[]");
     cL.pushFront(2);
     REQUIRE(cL.getSize() == 1);
-    REQUIRE(cL.toString() == "2");
+    REQUIRE(cL.toString() == "[2]");
     cL.pushFront(4);
     REQUIRE(cL.getSize() == 2);
-    REQUIRE(cL.toString() == "4 2");
+    REQUIRE(cL.toString() == "[4, 2]");
     cL.pushFront(6);
     REQUIRE(cL.getSize() == 3);
-    REQUIRE(cL.toString() == "6 4 2");
+    REQUIRE(cL.toString() == "[6, 4, 2]");
 }
 
 TEST_CASE("On 6-number list:", "[circlelist]")
@@ -33,43 +33,43 @@ TEST_CASE("On 6-number list:", "[circlelist]")
     cl.moveNext();
     cl.pushFront(16);
     cl.moveNext();
-    REQUIRE(cl.toString() == "11 12 13 14 15 16");
+    REQUIRE(cl.toString() == "[11, 12, 13, 14, 15, 16]");
 
     SECTION("Can advance 4 times") {
         for (int j = 0; j < 4; j++) 
             cl.moveNext();
-        REQUIRE(cl.toString() == "15 16 11 12 13 14");
+        REQUIRE(cl.toString() == "[15, 16, 11, 12, 13, 14]");
     }
 
     SECTION("Insert at position 2") {
         cl.moveNext();
         cl.moveNext();
-        REQUIRE(cl.toString() == "13 14 15 16 11 12");
+        REQUIRE(cl.toString() == "[13, 14, 15, 16, 11, 12]");
         cl.pushFront(1000);
-        REQUIRE(cl.toString() == "1000 13 14 15 16 11 12");
+        REQUIRE(cl.toString() == "[1000, 13, 14, 15, 16, 11, 12]");
         cl.moveNext();
         cl.moveNext();
         cl.moveNext();
         cl.moveNext();
         cl.moveNext();
-        REQUIRE(cl.toString() == "11 12 1000 13 14 15 16");
+        REQUIRE(cl.toString() == "[11, 12, 1000, 13, 14, 15, 16]");
     }
 
     SECTION("Delete at position 2") {
         cl.moveNext();
         cl.moveNext();
-        REQUIRE(cl.toString() == "13 14 15 16 11 12");
+        REQUIRE(cl.toString() == "[13, 14, 15, 16, 11, 12]");
         cl.popFront();
-        REQUIRE(cl.toString() == "14 15 16 11 12");
+        REQUIRE(cl.toString() == "[14, 15, 16, 11, 12]");
         cl.moveNext();
         cl.moveNext();
         cl.moveNext();
-        REQUIRE(cl.toString() == "11 12 14 15 16");
+        REQUIRE(cl.toString() == "[11, 12, 14, 15, 16]");
     }
 
     SECTION("insert, then delete") {
         cl.pushFront(100);
-        REQUIRE(cl.toString() == "100 11 12 13 14 15 16");
+        REQUIRE(cl.toString() == "[100, 11, 12, 13, 14, 15, 16]");
         cl.moveNext();
         cl.moveNext();
         cl.popFront();
@@ -77,7 +77,7 @@ TEST_CASE("On 6-number list:", "[circlelist]")
         cl.moveNext();
         cl.moveNext();
         cl.moveNext();
-        REQUIRE(cl.toString() == "100 11 13 14 15 16");
+        REQUIRE(cl.toString() == "[100, 11, 13, 14, 15, 16]");
     }
 
     SECTION("ability to delete everything")
@@ -85,12 +85,12 @@ TEST_CASE("On 6-number list:", "[circlelist]")
         cl.popFront();
         cl.popFront();
         cl.popFront();
-        REQUIRE(cl.toString() == "14 15 16");
+        REQUIRE(cl.toString() == "[14, 15, 16]");
         cl.popFront();
         cl.popFront();
         cl.popFront();
-        REQUIRE(cl.toString() == "");
+        REQUIRE(cl.toString() == "[]");
         cl.pushFront(17);
-        REQUIRE(cl.toString() == "17");
+        REQUIRE(cl.toString() == "[17]");
     }
 }
